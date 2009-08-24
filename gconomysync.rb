42bd6039fa97178c18264952c5f51b7b3f1f846d
David Yang <david.g.yang@gmail.com>
2009-08-24 15:54:32 -0400

    first draft

--- gconomysync.rb
+++ gconomysync.rb
@@ -1,28 +1,4 @@
 #!/usr/bin/ruby
-require 'logger'
-
-$LOG = Logger.new('gconomy.log', 'monthly')
-
-basedir="/chroots"
-
-partners = [ "kashless" ]
-sync_to_rb = [ "uploads" ]
-sync_from_rb = [ "log", "processed" ]
-sync_two_way = []
-
-scp_to
-
-partners.each do |p|
-  homedir = "#{basedir}/#{p}/home/#{p}/"
-  
-  # for each sync to RB, rsync files to remote machine, if successful, delete local files
-  
-  # for each sync from RB, rsync files, then if successful, ssh delete them
-  
-  # for two-way syncs, just run rsync
-
-
-#!/usr/bin/ruby
 require 'rubygems'
 require 'Logger'
 require 'benchmark'
@@ -30,25 +6,38 @@ require 'ping'
 require 'FileUtils'
 require 'open3'
 
+# Local Machine options === DIR OPTIONS ===
+
+localdir = "/chroots/kashless/home/kashless"
+remotedir = "/home/tech/gconomy/kashless"
+
+sync_uni_to = [ "uploads" ]
+sync_uni_from = [ "log", "processed" ]
+sync_two_way = []
+
+# === END OF DIR OPTIONS ===
+
+# == Options for the remote machine.
+
+SSH_USER      = 'tech'
+SSH_SERVER    = '192.168.1.145'
+SSH_PORT      = '' #Leave blank for default (port 22).
+RSYNC_VERBOSE = '-v'
+RSYNC_OPTS    = "--force --ignore-errors -az"
+RSYNC_ONE_WAY_OPTS = "--remove-source-files"
+
+
 #============================= OPTIONS ==============================#
 # == Options for local machine.
 SSH_APP       = 'ssh'
 RSYNC_APP     = 'rsync'
 
-EXCLUDE_FILE  = '/path/to/.rsyncignore'
-DIR_TO_BACKUP = '/folder/to/backup'
+#EXCLUDE_FILE  = '/path/to/.rsyncignore'
+#DIR_TO_BACKUP = '/folder/to/backup'
 LOG_FILE      = '/var/log/rrsync.log'
 LOG_AGE       = 'daily'
 
 EMPTY_DIR     = '/tmp/empty_rsync_dir/' #NEEDS TRAILING SLASH.
-# == Options for the remote machine.
-SSH_USER      = 'syncer'
-SSH_SERVER    = '192.168.1.145'
-SSH_PORT      = '' #Leave blank for default (port 22).
-BACKUP_ROOT   = '/path/on/remote/machine/to/backup/folder'
-BACKUP_DIR    = BACKUP_ROOT + '/' + Time.now.strftime('%A').downcase
-RSYNC_VERBOSE = '-v'
-RSYNC_OPTS    = "--force --ignore-errors --delete-excluded --exclude-from=#{EXCLUDE_FILE} --delete --backup --backup-dir=#{BACKUP_DIR} -a"
 # == Options to control output
 DEBUG         = true #If true output to screen else output is sent to log file.
 SILENT        = false #Total silent = no log or screen output.
@@ -62,28 +51,52 @@ else
   logger = Logger.new(nil)
 end
 ssh_port = SSH_PORT.empty? ? '' : "-e 'ssh -p #{SSH_PORT}'"
-rsync_cleanout_cmd = "#{RSYNC_APP} #{RSYNC_VERBOSE} #{ssh_port} --delete -a #{EMPTY_DIR} #{SSH_USER}@#{SSH_SERVER}:#{BACKUP_DIR}"
-rsync_cmd = "#{RSYNC_APP} #{RSYNC_VERBOSE} #{ssh_port} #{RSYNC_OPTS} #{DIR_TO_BACKUP} #{SSH_USER}@#{SSH_SERVER}:#{BACKUP_ROOT}/current"
+
+#rsync_cleanout_cmd = "#{RSYNC_APP} #{RSYNC_VERBOSE} #{ssh_port} --delete -a #{EMPTY_DIR} #{SSH_USER}@#{SSH_SERVER}:#{BACKUP_DIR}"
+#rsync_cmd = "#{RSYNC_APP} #{RSYNC_VERBOSE} #{ssh_port} #{RSYNC_OPTS} #{DIR_TO_BACKUP} #{SSH_USER}@#{SSH_SERVER}:#{BACKUP_ROOT}/current"
+
+rsync_cmd = "#{RSYNC_APP} #{RSYNC_VERBOSE} #{ssh_port} #{RSYNC_OPTS}"
+rsync_one_way_cmd = "#{rsync_cmd} #{RSYNC_ONE_WAY_OPTS}"
 
 logger.info("Started running at: #{Time.now}")
 run_time = Benchmark.realtime do
   begin
     raise Exception, "Unable to find remote host (#{SSH_SERVER})" unless Ping.pingecho(SSH_SERVER)
 
-    FileUtils.mkdir_p("#{EMPTY_DIR}")
-    Open3::popen3("#{rsync_cleanout_cmd}") { |stdin, stdout, stderr|
-      tmp_stdout = stdout.read.strip
-      tmp_stderr = stderr.read.strip
-      logger.info("#{rsync_cleanout_cmd}\n#{tmp_stdout}") unless tmp_stdout.empty?
-      logger.error("#{rsync_cleanout_cmd}\n#{tmp_stderr}") unless tmp_stderr.empty?
-    }
-    Open3::popen3("#{rsync_cmd}") { |stdin, stdout, stderr|
-      tmp_stdout = stdout.read.strip
-      tmp_stderr = stderr.read.strip
-      logger.info("#{rsync_cmd}\n#{tmp_stdout}") unless tmp_stdout.empty?
-      logger.error("#{rsync_cmd}\n#{tmp_stderr}") unless tmp_stderr.empty?
-    }
-    FileUtils.rmdir("#{EMPTY_DIR}")
+    # uni-to sync
+    sync_uni_to.each do |dir| 
+      rsync_uni_to = "#{rsync_one_way_cmd} #{localdir}/#{dir}/ #{SSH_USER}@#{SSH_SERVER}:#{remotedir}/#{dir}"
+      Open3::popen3("#{}") { |stdin, stdout, stderr|
+        tmp_stdout = stdout.read.strip
+        tmp_stderr = stderr.read.strip
+        logger.info("#{rsync_cmd}\n#{tmp_stdout}") unless tmp_stdout.empty?
+        logger.error("#{rsync_cmd}\n#{tmp_stderr}") unless tmp_stderr.empty?
+      }
+    end
+
+    # uni-from sync
+    sync_uni_from.each do |dir| 
+      rsync_uni_from = "#{rsync_one_way_cmd} #{SSH_USER}@#{SSH_SERVER}:#{remotedir}/#{dir} #{localdir}/#{dir}/"
+      Open3::popen3("#{}") { |stdin, stdout, stderr|
+        tmp_stdout = stdout.read.strip
+        tmp_stderr = stderr.read.strip
+        logger.info("#{rsync_cmd}\n#{tmp_stdout}") unless tmp_stdout.empty?
+        logger.error("#{rsync_cmd}\n#{tmp_stderr}") unless tmp_stderr.empty?
+      }
+    end
+
+    # bi sync
+    sync_uni_to.each do |dir| 
+      rsync_uni_to = "#{rsync_cmd} #{localdir}/#{dir}/ #{SSH_USER}@#{SSH_SERVER}:#{remotedir}/#{dir}"
+      Open3::popen3("#{}") { |stdin, stdout, stderr|
+        tmp_stdout = stdout.read.strip
+        tmp_stderr = stderr.read.strip
+        logger.info("#{rsync_cmd}\n#{tmp_stdout}") unless tmp_stdout.empty?
+        logger.error("#{rsync_cmd}\n#{tmp_stderr}") unless tmp_stderr.empty?
+      }
+    end
+    
+    
   rescue Errno::EACCES, Errno::ENOENT, Errno::ENOTEMPTY, Exception => e
     logger.fatal(e.to_s)
   end
